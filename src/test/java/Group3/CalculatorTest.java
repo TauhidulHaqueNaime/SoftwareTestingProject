db9d909bb0a8e3cefa6e89137da408c858fdd5e2
TauhidulHaqueNaime <mdna19@student.bth.se>
2020-04-21 22:16:29 +0200

    Initial Commit for testing

---
+++ src/test/java/Group3/CalculatorTest.java
@@ -0,0 +1,14 @@
+package Group3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void AddTest() {
+		fail("Not yet implemented");
+	}
+
+}
