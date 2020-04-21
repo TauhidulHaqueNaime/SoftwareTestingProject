13d75bdf4c8d1a0f5bc6f15b7701eb1d292b18e2
TauhidulHaqueNaime <mdna19@student.bth.se>
2020-04-22 00:41:04 +0200

    Calculator test cases added

--- src/main/java/Group3/Calculator.java
+++ src/main/java/Group3/Calculator.java
@@ -4,4 +4,23 @@ public class Calculator {
 	public int add(int a, int b) {
 		return a+b;
 	}
+	
+	public int sub(int a, int b) {
+		return 0;
+	}
+	
+	public int mul(int a, int b) {
+		return 0;
+	}
+	
+	public int div(int a, int b) {
+		if(b==0) {
+			throw new IllegalArgumentException("Number can not be divided by zero");
+		}
+		return 0;
+	}
+	
+	public int square(int a) {
+		return 0;
+	}
 }
---
+++ src/main/java/Group3/ConCat.java
@@ -0,0 +1,9 @@
+package Group3;
+
+public class ConCat {
+	
+	public String ConCating(String a, String b) {
+		return null;
+	}
+
+}
