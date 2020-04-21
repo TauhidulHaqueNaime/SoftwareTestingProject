6d5b012682662dc9c31846b8f64b21674aa45b67
TauhidulHaqueNaime <mdna19@student.bth.se>
2020-04-21 23:50:11 +0200

    modified

--- src/main/java/Group3/Calculator.java
+++ src/main/java/Group3/Calculator.java
@@ -4,23 +4,4 @@ public class Calculator {
 	public int add(int a, int b) {
 		return a+b;
 	}
-	
-	public int sub (int a, int b) {
-		return 0;
-	}
-	
-	public int mul (int a, int b) {
-		return 0;
-	}
-	
-	public int div (int a, int b) {
-		if (b==0) {
-			throw new IllegalArgumentException ("Number can not be divided by Zero!!");
-		}
-		return 0;
-	}
-	
-	public int square (int a) {
-		return 0;
-	}
 }
