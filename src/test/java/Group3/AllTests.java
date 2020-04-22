bfbc3cc828b0244e71af9eb2a6606cd614d0171e
TauhidulHaqueNaime <mdna19@student.bth.se>
2020-04-22 19:58:58 +0200

    All test suite added

---
+++ src/test/java/Group3/AllTests.java
@@ -0,0 +1,11 @@
+package Group3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CalculatorTest.class, ConCatTest.class, IntegrationTest.class, LockTest.class, LockUnlockTest.class })
+public class AllTests {
+
+}
